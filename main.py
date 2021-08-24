8522ab0923b403c449a0e8bd7d3098b0ce04d42a
Shruthi410 <46722803+Shruthi410@users.noreply.github.com>
2021-08-24 22:33:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import os, shutil
 
-dir = 'TicTacToeTests'
-delete_list = []
-for files in os.listdir(dir):
-    path = os.path.join(dir, files)
-    try:
-        shutil.rmtree(path)
-    except OSError:
-        os.remove(path)
-    delete_list.append(path)
+home_path = os.path.expanduser('~')
 
-print(f"Deleted files: {delete_list}")
+dir1 = os.path.join(home_path, r'Users/vajroprd/Library/Developer/Xcode/DerivedData')
+dir2 = os.path.join(home_path, r'Users/vajroprd/Library/Developer/Xcode/Archieves')
+dir3 = os.path.join(home_path, r'Library/Caches/')
+dir4 = os.path.join(home_path, r'Library/Application Support/')
+
+
+dirs = [dir1, dir2, dir3, dir4]
+
+
+for dir in dirs:
+    delete_list = []
+    for files in os.listdir(dir):
+        path = os.path.join(dir, files)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            os.remove(path)
+        delete_list.append(path)
+
+    print(f"Deleted files: {delete_list}")
+    
